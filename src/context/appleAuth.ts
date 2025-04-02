f9fe486b8ca23d0c3a5284b129db895da392e3bf
BurakDorman <burakdorman@gmail.com>
2025-04-02 16:18:53 +0300

    Updated encryption (sha256) for Apple Auth Removed reset-project.js

--- src/context/appleAuth.ts
+++ src/context/appleAuth.ts
@@ -1,19 +1,24 @@
 import * as AppleAuthentication from 'expo-apple-authentication';
 import { OAuthProvider, Auth, AuthCredential } from 'firebase/auth';
+import { sha256 } from 'js-sha256';
 
 export async function signInWithApple(auth: Auth): Promise<AuthCredential> {
   try {
+    const nonce = Math.random().toString(36).substring(2);
+    const hashedNonce = sha256(nonce);
+
     const credential = await AppleAuthentication.signInAsync({
       requestedScopes: [
         AppleAuthentication.AppleAuthenticationScope.FULL_NAME,
         AppleAuthentication.AppleAuthenticationScope.EMAIL,
       ],
+      nonce: hashedNonce,
     });
 
     const provider = new OAuthProvider('apple.com');
     const oAuthCredential = provider.credential({
       idToken: credential.identityToken!,
-      rawNonce: credential.nonce,
+      rawNonce: nonce,
     });
 
     return oAuthCredential;
